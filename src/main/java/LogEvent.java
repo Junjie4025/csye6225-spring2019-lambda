24b03ed526ec54257803d16e7ac19b39dbdca47b
AngLi2 <31733648+AngLi2@users.noreply.github.com>
2019-03-25 22:36:14 -0400

    implemented RequestHanadler version 1.0, do not know if any bugs exist

--- src/main/java/LogEvent.java
+++ src/main/java/LogEvent.java
@@ -1,25 +1,76 @@
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.*;
-import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
-import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.*;
+import org.apache.commons.lang.RandomStringUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Iterator;
 
 public class LogEvent implements RequestHandler<SNSEvent, Object> {
-    static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-    static DynamoDB dynamoDB = new DynamoDB(client);
-    static String tableName = "csye6225";
+
+    final static Regions REGION = Regions.US_EAST_1;
+    final static String TABLE_NAME = "csye6225";
+    final static String DYNAMO_KEY = "email";
+    final static String FROM_EMAIL = "liang@csye6225-spring2019-liang2.me";
 
     public Object handleRequest(SNSEvent request, Context context) {
+        context.getLogger().log("Invocation started: " + getTimeStamp());
+        context.getLogger().log("1: " + (request == null));
+        context.getLogger().log("2: " + (request.getRecords().size()));
+        context.getLogger().log(request.getRecords().get(0).getSNS().getMessage());
 
+        AmazonDynamoDB AmazonDynamoDBclient = AmazonDynamoDBClientBuilder.standard().withRegion(REGION).build();
+        DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBclient);
+        Table table = dynamoDB.getTable(TABLE_NAME);
 
+        String toEmail = request.getRecords().get(0).getSNS().getMessage().split(",")[0];
+        Item item = table.getItem(DYNAMO_KEY, toEmail);
+        String token = RandomStringUtils.random(20);
+
+        if (item == null) {
+            item = new Item().withPrimaryKey(DYNAMO_KEY, toEmail).with("token", token)
+                    .with("ttl", ((System.currentTimeMillis() / 1000 + 1200)));
+            PutItemOutcome outcome = table.putItem(item);
+            context.getLogger().log("Put successful: " + outcome.toString());
+        } else {
+            context.getLogger().log("Record Already Present");
+            return null;
+        }
+
+        String subject = "Reset Password Request";
+        StringBuilder emailBodyBuilder = new StringBuilder();
+        emailBodyBuilder.append("You are receiving this mail because to chose to reset your password.\n");
+        emailBodyBuilder.append("Please click the link below to Reset your password:\n");
+        emailBodyBuilder.append("Link: http://example.com/reset?email=" + toEmail + "&token=" + token+"\n");
+        emailBodyBuilder.append("This link will only be valid for 20 minuits starting: " + getTimeStamp());
+
+        try {
+            AmazonSimpleEmailService amazonSimpleEmailService = AmazonSimpleEmailServiceClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+            Destination destination = new Destination().withToAddresses(toEmail);
+            Content bodyContent = new Content().withCharset("UTF-8").withData(emailBodyBuilder.toString());
+            Content subContent = new Content().withCharset("UTF-8").withData(subject);
+            Message message = new Message().withBody(new Body().withHtml(bodyContent)).withSubject(subContent);
+            SendEmailRequest emailRequest = new SendEmailRequest().withDestination(destination).withMessage(message).withSource(FROM_EMAIL);
+            SendEmailResult result = amazonSimpleEmailService.sendEmail(emailRequest);
+            context.getLogger().log("Request result: " + result.toString());
+        } catch (Exception e) {
+            context.getLogger().log("Exception : " + e.getMessage());
+        }
+        context.getLogger().log("Lambda Completed: " + getTimeStamp());
         return null;
     }
 
+    private String getTimeStamp() {
+        return new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+    }
 }
